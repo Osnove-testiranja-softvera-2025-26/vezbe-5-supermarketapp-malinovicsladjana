f487c73c3e7b179abd5dbf22680d47c007daf411
malinovicsladjana <malinovicsladjana25@gmail.com>
2026-04-16 19:50:11 +0200

    vezbe 5

--- OTS2019_Supermarket.Test/CartTest.cs
+++ OTS2019_Supermarket.Test/CartTest.cs
@@ -8,8 +8,23 @@ using System.Threading.Tasks;
 
 namespace OTS_Supermarket.Test
 {
+    [TestFixture]
     public class CartTest
     {
+        [Test]
+        public void AddOneToCart_ShouldAddItemToCart_Success()
+        {
+            //ARRANGE preduslovi
+            Cart cart = new Cart();
+            cart.Size = 5;
+            Monitor monitor = new Monitor();
 
+            //ACT
+            cart.AddOneToCart(monitor);
+
+            //ASSERT
+            Assert.That(cart.Size, Is.EqualTo(6));
+            Assert.That(cart.Amount, Is.EqualTo(100));
+        }
     }
 }
